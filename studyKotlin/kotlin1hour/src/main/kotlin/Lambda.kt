a0b0f9b71e77e0e2c7395b589f52cde676f9cad0
motoralex73 <mo1or@mail.ru>
2022-05-22 21:45:54 +0400

    study kotlin

---
+++ studyKotlin/kotlin1hour/src/main/kotlin/Lambda.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val a = listOf(1,2,3,4,5)
+
+    a.forEach { e -> println(e) }
+    println( a.map { e -> e*2 } )
+    println( a.filter { e ->e % 2 == 0 } )
+    println( a.reduce { sum, e -> sum + e} ) //просуммирует все элементы списка
+
+    //a.sorted() // сортировка
+    //a.sortedDescending {it} //сортирует на убывание
+
+    a.any { it > 10 } //false, возвращает true, если есть хотя бы один элемент удовлетворяющий условию
+    a.all { it < 10 } //true, возвращает true, если все элементы удовлетворяют условию
+    a.sum() //суммирует все элементы в коллекции
+
+    val numbers = listOf(1,3,-4,-2,-11)
+    val (positive,negative) = numbers.partition { it > 0 } //делит коллекцию на 2 списка
+    println(positive)
+    println(negative)
+
+    val result = listOf("a","b","ba","ccc","as").groupBy { it.length }
+    println(result) //HashMap или LinkedHashMap
+
+}
--- studyKotlin/kotlin1hour/src/main/kotlin/WhenExpression.kt
+++ studyKotlin/kotlin1hour/src/main/kotlin/WhenExpression.kt
@@ -1,3 +1,15 @@
+fun testWhen(input : Any) {
+    when (input) {
+        1 -> println("Единица")
+        2 -> println("Двойка")
+        in 10..20 -> println("от 10 до 20")
+        is String -> println("Была введена строка длиной ${input.length}")
+        else -> println("Что-то еще")
+    }
+}
+
 fun main() {
-    
+    testWhen(1)
+    testWhen("String")
+    testWhen(15)
 }
